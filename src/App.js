0250a93d0c3e55ef6384f00e71e5e0637584998b
meetnileshjadhav <meetnileshjadhav@gmail.com>
2022-07-31 18:17:12 +0530

    added double click to change reminder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { BrowserRouter , Routes, Route } from 'react-router-dom';
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
+  const location= useLocation();
+  
   return (
     <div className="footer">
+        {location.pathname === '/' && <small className="reminder-click">(** Double click to chage Reminder)</small>}
         <p>Copyright &copy; NJ-Projects, 2022</p>
         <Link to="/about">About</Link>
     </div>
